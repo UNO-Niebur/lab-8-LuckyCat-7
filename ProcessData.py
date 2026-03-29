ec09cc6e73ee01840496d959af134a7822c8d326
LuckyCat-7 <victoriagreg20@gmail.com>
2026-03-29 14:57:43 +0000

    done with lab 8

--- ProcessData.py
+++ ProcessData.py
@@ -1,7 +1,7 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name:Tori Gregory
+#Date:3/29/26
+#Assignment:Lab 8
 
 import random
 
@@ -12,12 +12,37 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
-
-
-
+  for line in inFile:
+    data = line.split()
+    firstname = data[0]
+    lastname = data[1]
+    studentnum = data[3]
+    year = data[5]
+    major = data[6]
+    studentID = makeID(firstname, lastname, studentnum)
+    MAJyear = makeMajorYear(major, year)
+    output = lastname + ", " + firstname +", " + studentID + ", " + MAJyear + "\n"
+    outFile.write(output)
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
-
+def makeID(first, last, IDnum):
+  idlen = len(IDnum)
+  while len(last) < 5:
+    last = last + "X"
+  id = first[0] + last + IDnum[idlen - 3: ]
+  return id
+def makeMajorYear(major, year):
+  major = major[ :3].upper()
+  if year == "Freshman":
+    year = "FR"
+  elif year == "Sophomore":
+    year = "SO"
+  elif year == "Junior":
+    year = "JR"
+  elif year == "Senior":
+    year = "SR"
+  majoryear = major + "-" + year
+  return majoryear
 if __name__ == '__main__':
   main()
